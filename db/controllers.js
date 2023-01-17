203dac21475cbfeafec3c384073dcb057f98120e
alexmurdoch <almurdoch62@gmail.com>
2023-01-17 11:09:15 +0000

    "formatting"

--- db/controllers.js
+++ db/controllers.js
@@ -1,31 +1,27 @@
-
-const {fetchTopics, fetchArticles, fetchArticleById} = require('../models/models')
-
-
-
-
+const {
+  fetchTopics,
+  fetchArticles,
+  fetchArticleById,
+} = require("../models/models");
 
 const getTopics = (req, res) => {
-    fetchTopics().then((topics)=>{
-        
-        res.status(200).send({topics})
-    })
-}
+  fetchTopics().then((topics) => {
+    res.status(200).send({ topics });
+  });
+};
 
 const getArticles = (req, res) => {
-    fetchArticles().then((articles)=>{
-       
-        res.status(200).send({articles})
-    })
-}
+  fetchArticles().then((articles) => {
+    res.status(200).send({ articles });
+  });
+};
 
 const getArticleById = (req, res) => {
-const {article_id} = req.params
+  const { article_id } = req.params;
 
-    fetchArticleById(article_id).then((article)=>{
+  fetchArticleById(article_id).then((article) => {
+    res.status(200).send({ article });
+  });
+};
 
-        res.status(200).send({article})
-    })
-}
-
-module.exports = {getTopics, getArticles, getArticleById}
+module.exports = { getTopics, getArticles, getArticleById };
