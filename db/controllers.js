d9993d60f73855e13c99a23cccef150878bd86e1
alexmurdoch <almurdoch62@gmail.com>
2023-01-18 09:54:22 +0000

    "7-post comment"

--- db/controllers.js
+++ db/controllers.js
@@ -42,8 +42,8 @@ const getCommentsByArticleId = (req, res, next) => {
   const { article_id } = req.params;
 
   fetchCommentsByArticleId(article_id)
-    .then((article) => {
-      res.status(200).send({ article });
+    .then((comments) => {
+      res.status(200).send(comments);
     })
     .catch((err) => {
       next(err);
